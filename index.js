ab647273843f80871ebeb01fdc035c715d95e741
Tom Nielsen <tanielsen@gmail.com>
2020-10-29 22:13:05 +0000

    rm results view

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const searchbar = require("./searchbar_view");
-const results = require("./results_view");
 
 module.exports = {
   sc_plugin_api_version: 1,
-  viewtemplates: [searchbar, results],
+  viewtemplates: [searchbar],
 };
